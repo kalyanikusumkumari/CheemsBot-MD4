1b881d158ce0a1b57777e1c3e6e18e4d174dcbef
kalyanikusumkumari <109850498+kalyanikusumkumari@users.noreply.github.com>
2022-07-23 18:19:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,15 +61,15 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['077037874']
+global.ownertag = '0770378874' //ur tag number
+global.botname = '𝐒𝐔𝐍𝐄𝐓𝐇 𝐁𝐎𝐓 ' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.packname = "Cheems Bot 𝐬𝐮𝐧𝐞𝐭𝐡 Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
 global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
 
 //Bot theme media
